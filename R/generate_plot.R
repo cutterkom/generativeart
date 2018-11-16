cc7502044c1b1b7093811e6cfc020f20d7739030
Katharina Brunner <mail@katharina-brunner.degit config --global user.email mail@katharina-brunner.degit config --global user.name kabr>
2018-11-16 11:46:03 +0100

    aes ggplot2

--- R/generate_plot.R
+++ R/generate_plot.R
@@ -16,14 +16,14 @@ generate_plot <- function(df, file_name, polar) {
   print("generate plot")
   if (polar == TRUE) {
     plot <- df %>%
-      ggplot2::ggplot(aes(x = x, y = y)) +
+      ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
       ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20) +
       ggplot2::theme_void() +
       ggplot2::coord_fixed() +
       ggplot2::coord_polar()
   } else {
     plot <- df %>%
-      ggplot2::ggplot(aes(x = x, y = y)) +
+      ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
       ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20) +
       ggplot2::theme_void() +
       ggplot2::coord_fixed()
