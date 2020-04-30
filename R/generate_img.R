41489729cd5c08dfb63393db987436eeb568fa40
Nico Hahn <Nico Hahn>
2020-04-30 12:00:38 +0200

    added the ability to select color and background color

--- R/generate_img.R
+++ R/generate_img.R
@@ -5,6 +5,7 @@
 #' @param polar logical should the plot have a polar coordinate system ("polar = TRUE") or a cartesian coordinate system ("polar = FALSE")
 #' @param nr_of_img the number of images that should be created
 #' @param file_name filetype of the final image. Default is `png`, for other options see the `devics` argument in `gggplot::gsave()`
+#' @param ... further arguments like color and background_color
 #' @return as many png files as you net in "nr_of_img"
 #' @seealso \code{\link{generate_seeds}} generate the seeds for the randomness
 #' @seealso \code{\link{generate_filename}} generate the file names
@@ -15,9 +16,10 @@
 #' @export
 #' @examples
 #' generate_img(formula = my_formula, nr_of_img = 3, polar = FALSE)
+#' generate_img(formula = my_formula, nr_of_img = 3, polar = FALSE, color = "#101820", background_color = "#F2AA4C")
 #' @importFrom purrr map
 
-generate_img <- function(formula, nr_of_img, polar = FALSE, filetype = "png") {
+generate_img <- function(formula, nr_of_img, polar = FALSE, filetype = "png", ...) {
   seeds <- generate_seeds(nr_of_img)
   purrr::map(seeds, function(seed){
     set.seed(seed)
@@ -25,6 +27,6 @@ generate_img <- function(formula, nr_of_img, polar = FALSE, filetype = "png") {
     logfile <- check_logfile_existence()
     logfile <- generate_logfile_entry(logfile, formula, seed, file_name)
     df <- generate_data(formula)
-    plot <- generate_plot(df, file_name, polar, filetype)
+    plot <- generate_plot(df, file_name, polar, filetype, ...)
   })
 }
--- R/generate_plot.R
+++ R/generate_plot.R
@@ -5,6 +5,8 @@
 #' @param file_name the file name for saving the plot
 #' @param polar do you want to use a polar coordinate system? The default is a cartesian coordinate system.
 #' @param file_name filetype of the final image. Default is `png`, for other options see the `devics` argument in `gggplot::gsave()`
+#' @param color color of the points. default is black
+#' @param background_color background color of the plaut. default is white.
 #' @return a png file
 #' @seealso \code{\link{generate_data}} where the data is created
 #' @export
@@ -13,21 +15,29 @@
 #' @import ggplot2
 #' @importFrom magrittr %>%
 
-generate_plot <- function(df, file_name, polar, filetype) {
+generate_plot <- function(df, file_name, polar, filetype, color = "black", background_color = "white") {
   print("generate plot")
   if (polar == TRUE) {
     plot <- df %>%
       ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
-      ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20) +
+      ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20, color = color) +
       ggplot2::theme_void() +
       ggplot2::coord_fixed() +
-      ggplot2::coord_polar()
+      ggplot2::coord_polar() +
+      ggplot2::theme(
+        panel.background = element_rect(fill = background_color),
+        plot.background = element_rect(fill = background_color)
+        )
   } else {
     plot <- df %>%
       ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
-      ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20) +
+      ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20, color = color) +
       ggplot2::theme_void() +
-      ggplot2::coord_fixed()
+      ggplot2::coord_fixed() +
+      ggplot2::theme(
+        panel.background = element_rect(fill = background_color),
+        plot.background = element_rect(fill = background_color)
+        )
   }
   ggplot2::ggsave(plot, filename = paste0(IMG_PATH, file_name), width = 6, height = 6, device = filetype)
   print("image saved...")
